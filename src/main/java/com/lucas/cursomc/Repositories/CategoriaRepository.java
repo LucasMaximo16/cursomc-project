362a1c74ef8d1722e32df1cf4236ea0459d4ecaf
LucasMaximo16 <lucasmaximodeandrade@gmail.com>
2022-09-01 03:23:56 -0300

    Inserir dados no banco, criar Interface Repository e Classe Service, Arrumando o Resource

---
+++ src/main/java/com/lucas/cursomc/Repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.lucas.cursomc.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lucas.cursomc.domain.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
---
+++ src/main/java/com/lucas/cursomc/Service/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.lucas.cursomc.Service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucas.cursomc.Repositories.CategoriaRepository;
+import com.lucas.cursomc.domain.Categoria;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository catRepository;
+	
+	public Categoria buscar(Integer id) {
+		
+		Optional<Categoria> cat = catRepository.findById(id);
+		
+		return cat.orElse(null);
+	}
+}
