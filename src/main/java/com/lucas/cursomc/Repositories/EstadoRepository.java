38bc437597c6e9a8cd89c5bb9eca5e65bf13a50d
LucasMaximo16 <lucasmaximodeandrade@gmail.com>
2022-09-05 03:36:17 -0300

    Adcionando Estado e Cidade

---
+++ src/main/java/com/lucas/cursomc/Repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.lucas.cursomc.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lucas.cursomc.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
